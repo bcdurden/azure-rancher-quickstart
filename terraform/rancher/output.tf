2f0d86dbe8913ae99650f2e7db1343f6f15cebec
Brian Durden <brian.durden@rancherfederal.com>
2023-03-07 15:32:04 -0500

    added rancher lb ip

--- terraform/rancher/output.tf
+++ terraform/rancher/output.tf
@@ -2,3 +2,9 @@ output "rancher_node_ip" {
   value = azurerm_linux_virtual_machine.rancher_server.public_ip_address
 }
 
+output "rancher_lb_ip" {
+  value = azurerm_public_ip.rancher_lb_ip.ip_address
+}
+output "rancher_dns" {
+  value = var.rancher_server_dns
+}
